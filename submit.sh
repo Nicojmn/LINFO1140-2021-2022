c5b64bc02e52ca75058a4ab71453545ae3fcbef6
nicomjn <nicolasjeanmenne@gmail.com>
2022-02-13 20:38:47 +0100

    Travail1 done

--- submit.sh
+++ submit.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 DIR=$1
-ROOT="/home/nicolas/UCL/BAC1-Q2/LINFO1140-electronique/travaux/2021-2022/LINFO1140-2021-2022"
+NUM=$2
 
 if [ -d "$DIR" ]; then
-    zip -r ./"$DIR"/"$DIR"-Simu-Jeanmenne-Nicolas.zip ./"$DIR"/simu
+    cd ./"$DIR"/simu
+    zip -r ./"$DIR"-Simu-Jeanmenne-Nicolas.zip ./*
+    cd ../..
     cp -a "$DIR"/build_latex/main.pdf "$DIR"/
-    mv "$DIR"/main.pdf "$DIR"/Rapport-Jeanmenne-Nicolas.pdf
-    find "$ROOT" -name "*.log" -or -name "*.aux" -name "*.fls" -name "*.maf" -name "*.mtc*" -name "*.out" -name "*.synctex*" -delete 
+    mv "$DIR"/main.pdf "$DIR"/Rapport"$NUM"-Jeanmenne-Nicolas.pdf
 else
     echo "Directory not found, aborting." && exit 1
 fi
